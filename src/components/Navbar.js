abd4b91a8e2f392f9b5e97e1e46d4ce7c10d7c7e
Jesus Vazquez <jesusvz37@gmail.com>
2022-03-30 10:53:17 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar has-shadow"
         role="navigation"
         aria-label="main-navigation"
       >
